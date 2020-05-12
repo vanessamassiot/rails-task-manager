dc069dd423d4e91e1aea0bac6d9a09034195e000
vanessa massiot <vanessa.massiot@free.fr>
2020-05-12 11:59:07 +0200

    show task

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "tasks", to: "tasks#index"
 
+  get "tasks/:id", to: "tasks#show", as: :task
+
 
 
 end
